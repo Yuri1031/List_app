4f69e40d59b6547e1279832f05d2a93ccce31a30
Yuri1031 <142709785+Yuri1031@users.noreply.github.com>
2025-09-06 13:37:09 +0900

    追加実装

--- JS/order.js
+++ JS/order.js
@@ -1,21 +1,31 @@
 console.log('connected to order.js!');
 
 document.addEventListener("DOMContentLoaded", () => {
-    const list = document.querySelectorAll(".todo_lists");
+    const list = document.querySelector(".todo_lists");
+    const tasks = document.querySelectorAll(".task");
 
-    list.forEach(list => {
-        list.addEventListener("dragstart", e => {
-            if(e.target.classList.contains("task")){
-                e.target.classList.add("dragging");
-            }
+    tasks.forEach(task => {
+        task.addEventListener("dragstart", e => {
+            e.target.classList.add("drag");
         });
 
-        list.addEventListener("dragend", e => {
-            if(e.target.classList.contains("task")){
-                e.target.classList.remove("dragging");
-            }
-        });
+        task.addEventListener("dragend", e => {
+            e.target.classList.remove("drag");
+        });        
+    })
 
-        list.addEventListener("dragover", e => {});
+    list.addEventListener("dragover", e => {
+        e.preventDefault();
+        // drag対象物
+        const dragTask = document.querySelector(".drag");
+        // drag対象物以外（ドロップされる候補）※ nodeListだとfindメソッドが使用不可のためスプレッド構文で配列にする。
+        let anotherList = [...list.document.querySelectorAll(".task:not(.drag)")];
+        // drag対象物以外（ドロップされるもの）
+        let dropped = anotherList.find((another)=>{
+            // dragした際に対象物が乗った候補
+            return e.clientY <= another.offsetTop + another.offsetHeight / 2;
+        });
+        list.insertBefore(dragTask, dropped);
     })
 })
+

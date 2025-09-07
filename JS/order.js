25c32901bde26da40be5aad64463160b37ca7cf9
Yuri1031 <142709785+Yuri1031@users.noreply.github.com>
2025-09-07 12:08:01 +0900

    追加実装

--- JS/order.js
+++ JS/order.js
@@ -1,31 +1,32 @@
 console.log('connected to order.js!');
 
 document.addEventListener("DOMContentLoaded", () => {
-    const list = document.querySelector(".todo_lists");
-    const tasks = document.querySelectorAll(".task");
+    const lists = document.querySelectorAll(".todo_lists"); // 全カテゴリー取得
 
-    tasks.forEach(task => {
-        task.addEventListener("dragstart", e => {
+    lists.forEach(list => {
+        list.addEventListener("dragstart", e => {
             e.target.classList.add("drag");
         });
 
-        task.addEventListener("dragend", e => {
+        list.addEventListener("dragend", e => {
             e.target.classList.remove("drag");
-        });        
-    })
-
-    list.addEventListener("dragover", e => {
-        e.preventDefault();
-        // drag対象物
-        const dragTask = document.querySelector(".drag");
-        // drag対象物以外（ドロップされる候補）※ nodeListだとfindメソッドが使用不可のためスプレッド構文で配列にする。
-        let anotherList = [...list.document.querySelectorAll(".task:not(.drag)")];
-        // drag対象物以外（ドロップされるもの）
-        let dropped = anotherList.find((another)=>{
-            // dragした際に対象物が乗った候補
-            return e.clientY <= another.offsetTop + another.offsetHeight / 2;
         });
-        list.insertBefore(dragTask, dropped);
-    })
+
+        list.addEventListener("dragover", e => {
+            e.preventDefault();
+            // drag対象物
+            const dragTask = document.querySelector(".drag");
+
+            // drag対象物以外（ドロップされる候補）※ nodeListだとfindメソッドが使用不可のためスプレッド構文で配列にする。
+            let anotherList = [...list.querySelectorAll(".task:not(.drag)")];
+
+            // drag対象物以外（ドロップされるもの）
+            let dropped = anotherList.find((another)=>{
+                // dragした際に対象物が乗った候補
+                return e.clientY <= another.offsetTop + another.offsetHeight / 2;
+            });
+            list.insertBefore(dragTask, dropped);
+        })
+    });
 })
 
